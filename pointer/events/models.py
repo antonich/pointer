2b84b99d96fcbb975eb7620777d262d453058d5f
StasKarpov <karpovstanislav1@gmail.com>
2017-08-14 14:30:35 +0300

    Tests for events

--- pointer/events/models.py
+++ pointer/events/models.py
@@ -1,10 +1,42 @@
 from django.db import models
 from django.conf import settings
-from friends.models import FriendshipManager
+from friends.models import Friendship
+import datetime
 
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
+class PointerManager(models.Manager):
+    def create_pointer(self, creator, name, date, description, is_private, invited_people=None):
+        if date < datetime.datetime.now():
+            raise ValueError
+
+        pointer = self.create(
+            name=name,
+            date=date,
+            description=description,
+            is_private=is_private
+        )
+        #creates admin
+        Member.objects.create_member(user=creator, pointer=pointer, is_accepted=True, is_admin=True)
+        if is_private:#inviting another people
+            if invited_people:
+                for user in invited_people:
+                    Member.objects.create_member(user=user, pointer=pointer, is_accepted=False, is_admin=False)
+        pointer.save(using=self._db)
+        return pointer
+
+    def get_suggested_pointerlist(self, user):
+        """returns all public pointers that friends are going to visit"""
+        friendslist = Friendship.objects.friends_list(user)
+        pointers = list()
+        for user in friendslist:
+            user_pointers = Member.objects.get_planned_pointerslist(user)
+            for up in user_pointers:
+                pointers.append(up)
+        #now we have pointers and we have to remove duplicates and sort it by duplicate quantity
+        #todo
+        return pointers
 
 class Pointer(models.Model):
     """Model to represent events"""
@@ -12,32 +44,30 @@ class Pointer(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=500)
     is_private = models.BooleanField(default=False)
-    object = PointerManager()
+    objects = PointerManager()
 
-class PointerManager(models.Manager):
-    def create_pointer(self, creator, name, date, description, is_private, invited_people=None):
-        pointer = self.model(
-            name=name,
-            date=date,
-            description=description,
-            is_private=is_private
+class MembersManager(models.Manager):
+    def create_member(self, user, pointer, is_accepted, is_admin=False):
+        """invite someone"""
+        member = self.create(
+            user=user,
+            pointer=pointer,
+            is_accepted=is_accepted,
+            is_admin=is_admin
         )
-        #creates admin
-        MembersManager.create_member(user=creator, pointer=pointer, is_accepted=True, is_admin=True)
-        if is_private:#inviting another people
-            for user in invited_people:
-                MembersManager.create_member(user=user, pointer=pointer, is_accepted=False, is_admin=False)
-        return pointer
+        member.save(using=self._db)
+        return member
 
-    def get_suggested_pointerlist(self, user):
-        friendslist = FriendshipManager.friends_list(user)
+    def get_memberslist(self, pointer):
+        """returns memberlist for certain pointer"""
+        return [i.user for i in Member.objects.filter(pointer=pointer)]
+
+    def get_planned_pointerslist(self, member):
+        """returns list of planned public pointers"""
         pointers = list()
-        for user in friendslist:
-            user_pointers = MembersManager.get_planned_pointerslist(user)
-            for up in user_pointers:
-                pointers.append(up)
-        #now we have pointers and we have to remove duplicates and sort it by duplicate quantity
-        #todo
+        for i in Member.objects.filter(user=member):
+            if not i.pointer.is_private:
+                pointers.append(i.pointer)
         return pointers
 
 class Member(models.Model):
@@ -51,34 +81,13 @@ class Member(models.Model):
         Pointer,
         related_name='event'
     )
-    is_accepted = models.BooleanField()
+    is_accepted = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    object = MembersManager()
+    objects = MembersManager()
 
     def accept_membership(self):
         self.is_accepted = True
 
-class MembersManager(models.Manager):
-    def create_member(self, user, pointer, is_accepted):
-        """invite someone"""
-        member = self.model(
-            user=user,
-            pointer=pointer,
-            is_accepted=is_accepted
-        )
-        return member
-
-    def get_memberslist(self, pointer):
-        """returns memberlist for certain pointer"""
-        return [i.user for i in Member.objects.filter(pointer=pointer)]
-
-    def get_planned_pointerslist(self, member):
-        """returns list of planned public pointers"""
-        list = list()
-        for i in Member.objects.filter(user=member):
-            if not i.pointer.is_private:
-                list.append(i.pointer)
-        return list
 
 
 
---
+++ pointer/events/tests/test_events.py
@@ -0,0 +1,107 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from users.models import User
+from events.models import *
+from friends.models import Request, Friendship
+
+
+class TestEvents(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="User1", email="email1")
+
+
+    def test_creating_of_public_pointer(self):
+        pointer = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer',
+                                                date=datetime.datetime.now()+datetime.timedelta(days=1),
+                                                description="This is a test pointer",
+                                                is_private=False)
+        self.assertEqual(Pointer.objects.count(), 1)
+
+    def test_joining_public_pointer(self):
+        pointer = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer',
+                                                date=datetime.datetime.now()+datetime.timedelta(days=1),
+                                                description="This is a test pointer",
+                                                is_private=False)
+
+        self.user2 = User.objects.create_user(username="User2", email="email2")
+        Member.objects.create_member(user=self.user2, pointer=pointer, is_accepted=True)
+
+        self.assertEqual(Member.objects.all().count(), 2)
+
+    def test_creating_private_pointer(self):
+        self.user2 = User.objects.create_user(username="User2", email="email2")
+        self.user3 = User.objects.create_user(username="User3", email="email3")
+
+        pointer = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer',
+                                                date=datetime.datetime.now() + datetime.timedelta(days=1),
+                                                description="This is a test pointer",
+                                                is_private=True,
+                                                invited_people=[self.user2, self.user3])
+
+        self.assertEqual(Pointer.objects.all().count(), 1)
+        self.assertEqual(Member.objects.all().count(), 3)
+
+    def test_get_member_list(self):
+        self.user2 = User.objects.create_user(username="User2", email="email2")
+        self.user3 = User.objects.create_user(username="User3", email="email3")
+        pointer = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer',
+                                                date=datetime.datetime.now() + datetime.timedelta(days=1),
+                                                description="This is a test pointer",
+                                                is_private=True,
+                                                invited_people=[self.user2, self.user3] )
+        members = Member.objects.get_memberslist(pointer)
+        self.assertEqual(len(members), 3)
+        self.assertTrue(self.user3 in members, True)
+        self.assertTrue(self.user2 in members, True)
+        self.assertTrue(self.user1 in members, True)
+
+    def test_check_if_creator_is_member(self):
+        pointer1 = Pointer.objects.create_pointer(creator=self.user1,
+                                                  name='Test pointer',
+                                                  date=datetime.datetime.now() + datetime.timedelta(days=1),
+                                                  description="This is a test pointer",
+                                                  is_private=True)
+
+        pointer2 = Pointer.objects.create_pointer(creator=self.user1,
+                                                  name='Test pointer2',
+                                                  date=datetime.datetime.now() + datetime.timedelta(days=2),
+                                                  description="This is a test pointer",
+                                                  is_private=True)
+        self.assertTrue(Member.objects.filter(user=self.user1).all().count(), 2)
+
+
+    def test_get_planned_poinetrlist(self):
+        pointer1 = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer',
+                                                date=datetime.datetime.now() + datetime.timedelta(days=1),
+                                                description="This is a test pointer",
+                                                is_private=False )
+
+        pointer2 = Pointer.objects.create_pointer(creator=self.user1,
+                                                name='Test pointer2',
+                                                date=datetime.datetime.now() + datetime.timedelta(days=2),
+                                                description="This is a test pointer",
+                                                is_private=False)
+        pointers = Member.objects.get_planned_pointerslist(member=self.user1)
+        self.assertEqual(len(pointers), 2)
+        self.assertTrue(pointer1 in pointers)
+        self.assertTrue(pointer2 in pointers)
+
+    def test_get_suggested_pointerlist(self):
+        self.user2 = User.objects.create_user(username="User2", email="email2")
+        Request.objects.send_request(from_user=self.user1, to_user=self.user2)
+        Request.objects.filter(from_user=self.user1)[0].accept()
+
+        pointer1 = Pointer.objects.create_pointer(creator=self.user1,
+                                                 name='Test pointer',
+                                                 date=datetime.datetime.now() + datetime.timedelta(days=1),
+                                                 description="This is a test pointer",
+                                                 is_private=False)
+
+        pointers = Pointer.objects.get_suggested_pointerlist(user=self.user2)
+
+        self.assertTrue(pointer1 in pointers, True)
